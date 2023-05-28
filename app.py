989e5d560b0a3d577048b3b91adffd88237d7381
raihansyahyoga <raihansyahyoga@gmail.com>
2023-05-29 00:23:00 +0700

    connected to node js through api

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/flask', methods=['GET'])
+def index():
+    return "Flask server"
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
