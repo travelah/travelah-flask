eeab434d65a52e0330cf03e533723b0377abf445
Qais Jabbar Ihkamuddin <qaisjabbar004@gmail.com>
2023-06-06 07:29:59 +0700

    fix req

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from keras.models import load_model
+from tf.keras.models import load_model
 import numpy as np
 import tensorflow_hub as hub
 import tensorflow_text as text
